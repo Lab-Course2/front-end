34e736f8b4114e40d35ff5d2b06362d043872bba
ZamiraAhm <zamiraahm1@gmail.com>
2024-04-27 18:56:44 +0200

    Added Login and Register Form

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,47 +1,70 @@
 import React, { useState } from 'react';
-import './Css/Register.css';
 
-const LoginForm = () => {
-    const [formData, setFormData] = useState({
-      email: '',
-      password: '',
+const LoginForm = ({handleLogin}) => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
-  
-    const handleInputChange = (e) => {
-      const { name, value } = e.target;
-      setFormData({
-        ...formData,
-        [name]: value,
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://localhost:7190/api/Authentication/SignIn', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
       });
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Implement your login logic here
-      console.log('Form submitted with data:', formData);
-      // You may want to add further logic for authentication
-    };
-  
-    return (
-      <div className="container">
-        <div className="card mt-4">
-          <div className="card-body">
-            <h2 className="mb-4 text-center">Login</h2>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="email" className="form-label">Email</label>
-              <input type="email" className="form-input" id="email" name="email" value={formData.email} onChange={handleInputChange} required />
-  
-              <label htmlFor="password" className="form-label">Password</label>
-              <input type="password" className="form-input" id="password" name="password" value={formData.password} onChange={handleInputChange} required />
-  
-              <div className="d-flex mt-3">
-                <button type="submit" className="btn btn-submit">Login</button>
-              </div>
-            </form>
-          </div>
+
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        const { token, role, userId } = jsonResponse;
+
+        localStorage.setItem('token', token);
+        localStorage.setItem('userId', userId);
+        localStorage.setItem('role', role);
+        
+        handleLogin(role, token, userId);
+        console.log('Login successful');
+      } else {
+        // Handle unsuccessful login, display error message, etc.
+        console.error('Login failed');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="card mt-4">
+        <div className="card-body">
+          <h2 className="mb-4 text-center">Login</h2>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username" className="form-label">Username</label>
+            <input type="text" className="form-input" id="username" name="username" value={formData.username} onChange={handleInputChange} required />
+
+            <label htmlFor="password" className="form-label">Password</label>
+            <input type="password" className="form-input" id="password" name="password" value={formData.password} onChange={handleInputChange} required />
+
+            <div className="d-flex mt-3">
+              <button type="submit" className="btn btn-submit">Login</button>
+            </div>
+          </form>
         </div>
       </div>
-    );
-  };
-  
-  export default LoginForm;
+    </div>
+  );
+};
+
+export default LoginForm;
