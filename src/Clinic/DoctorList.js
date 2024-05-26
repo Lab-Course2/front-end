9c0a15a62c8f2728b014bad5457e40b0ae40f8a1
ArtinTopojani <artintopojani2002@gmail.com>
2024-05-26 16:10:14 +0200

    Implemented Doctors Profile

--- src/Clinic/DoctorList.js
+++ src/Clinic/DoctorList.js
@@ -3,41 +3,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 
-const DoctorList = ({ userId }) => {
+const DoctorList = ({ userId }) =>
+{
   const [doctors, setDoctors] = useState([]);
   const [clinics, setClinics] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
+  useEffect(() =>
+  {
+    const fetchData = async () =>
+    {
+      try
+      {
         // Fetch doctors
-        const doctorsResponse = await fetch(
-          'https://localhost:7190/api/Doctor/GetAllDoctors'
-        );
-        if (doctorsResponse.ok) {
+        const doctorsResponse = await fetch('https://localhost:7190/api/Doctor/GetAllDoctors');
+        if (doctorsResponse.ok)
+        {
           const doctorsData = await doctorsResponse.json();
 
           // Filter doctors based on ClinicId matching userId
-          const filteredDoctors = doctorsData.filter(
-            (doctor) => doctor.clinicId === userId
-          );
+          const filteredDoctors = doctorsData.filter((doctor) => doctor.clinicId === userId);
 
           setDoctors(filteredDoctors);
-        } else {
+        } else
+        {
           console.error('Failed to fetch doctors:', doctorsResponse.statusText);
         }
 
         // Fetch clinics
-        const clinicsResponse = await fetch(
-          'https://localhost:7190/api/Clinic/GetAllClinics'
-        );
-        if (clinicsResponse.ok) {
+        const clinicsResponse = await fetch('https://localhost:7190/api/Clinic/GetAllClinics');
+        if (clinicsResponse.ok)
+        {
           const clinicsData = await clinicsResponse.json();
           setClinics(clinicsData);
-        } else {
+        } else
+        {
           console.error('Failed to fetch clinics:', clinicsResponse.statusText);
         }
-      } catch (error) {
+      } catch (error)
+      {
         console.error('Error during fetch:', error);
       }
     };
@@ -45,48 +48,52 @@ const DoctorList = ({ userId }) => {
     fetchData();
   }, [userId]);
 
-  const getClinicName = (clinicId) => {
+  const getClinicName = (clinicId) =>
+  {
     const clinic = clinics.find((c) => c.id === clinicId);
     return clinic ? clinic.name : 'Unknown Clinic';
   };
 
-  const handleDelete = async (id) => {
-    try {
-      const response = await fetch(
-        `https://localhost:7190/api/Doctor/DeleteDoctor?doctorId=${id}`,
-        {
-          method: 'DELETE',
-        }
-      );
+  const handleDelete = async (id) =>
+  {
+    try
+    {
+      const response = await fetch(`https://localhost:7190/api/Doctor/DeleteDoctor?doctorId=${id}`, {
+        method: 'DELETE',
+      });
 
-      if (response.ok) {
+      if (response.ok)
+      {
         setDoctors(doctors.filter((doctor) => doctor.id !== id));
-      } else {
+      } else
+      {
         console.error('Failed to delete doctor:', response.statusText);
       }
-    } catch (error) {
+    } catch (error)
+    {
       console.error('Error during delete:', error);
     }
   };
 
-  const handleEdit = (id) => {
+  const handleEdit = (id) =>
+  {
     // Use navigate function to navigate to the EditDoctor page and pass the doctor's ID as state
     // Adjust the route according to your application structure
     window.location.href = `/edit-doctor/${id}`;
   };
 
   return (
-    <div className='container mt-4'>
-      <div className='row'>
-        <div className='col-md-3' style={{ width: '25%' }}>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-md-3" style={{ width: '25%' }}>
           <Sidebar userRole='Clinic' />
         </div>
-        <div className='col-md-9' style={{ marginLeft: 'auto', width: '75%' }}>
+        <div className="col-md-9" style={{ marginLeft: 'auto', width: '75%' }}>
           <h2>Doctors List</h2>
-          <Link to='/create-doctor' className='btn btn-primary mb-3'>
+          <Link to="/create-doctor" className="btn btn-primary mb-3">
             Create Doctor
           </Link>
-          <table className='table table-striped'>
+          <table className="table table-striped">
             <thead>
               <tr>
                 <th>Username</th>
@@ -98,6 +105,7 @@ const DoctorList = ({ userId }) => {
                 <th>Date of Birth</th>
                 <th>Gender</th>
                 <th>Address</th>
+                <th>Description</th>
                 <th>Clinic Name</th>
                 <th>Action</th>
               </tr>
@@ -114,18 +122,13 @@ const DoctorList = ({ userId }) => {
                   <td>{doctor.dateOfBirth}</td>
                   <td>{doctor.gender}</td>
                   <td>{doctor.address}</td>
+                  <td>{doctor.description}</td>
                   <td>{getClinicName(doctor.clinicId)}</td>
                   <td>
-                    <Link
-                      to={`/edit-doctor/${doctor.id}`}
-                      className='btn btn-primary ml-2'
-                    >
+                    <Link to={`/edit-doctor/${doctor.id}`} className="btn btn-primary ml-2">
                       Edit
                     </Link>
-                    <button
-                      className='btn btn-danger'
-                      onClick={() => handleDelete(doctor.id)}
-                    >
+                    <button className="btn btn-danger" onClick={() => handleDelete(doctor.id)}>
                       Delete
                     </button>
                   </td>
